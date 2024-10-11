3d4a837a687c49dd16f16e536f510bf24de157e5
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-11 16:42:00 +0300

    Add catch error / posts variable in render

--- react-axios/src/Components/HTTPRequests.jsx
+++ react-axios/src/Components/HTTPRequests.jsx
@@ -10,18 +10,42 @@ export class HTTPRequests extends Component {
     };
   }
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      console.log(response);
-      this.setState({
-        posts: response.data,
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          posts: Array.isArray(response.data) ? response.data : [response.data],
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message,
+        });
       });
-    });
   }
+
   render() {
+    const posts = this.state.posts;
     return (
       <div>
         <h2>Posts: </h2>
-        {JSON.stringify(this.state.posts)}
+        {posts.length ? (
+          posts.map((post) => (
+            <div key={post.id}>
+              <h2>
+                {post.id}. {post.title}
+              </h2>
+              <h4>By User ID {post.userid}</h4>
+              <p>{post.body}</p>
+              <hr />
+            </div>
+          ))
+        ) : this.state.error ? (
+          <p>{this.state.error}</p>
+        ) : (
+          <h4>Loading...</h4>
+        )}
       </div>
     );
   }
