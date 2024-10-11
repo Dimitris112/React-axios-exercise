ca89016295f672ea5165de5df6ed719d58087258
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-11 16:00:58 +0300

    Add HTTPRequests component - rce / rcon

---
+++ react-axios/src/Components/HTTPRequests.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export class HTTPRequests extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h2>Posts: </h2>
+      </div>
+    );
+  }
+}
+
+export default HTTPRequests;
