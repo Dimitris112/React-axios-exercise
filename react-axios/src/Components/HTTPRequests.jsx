f932d6dc42c7a455ee08f002c6f76e9d6cd0ce5c
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-11 16:13:09 +0300

    Add component did mount to get json response / json stringify in render

--- react-axios/src/Components/HTTPRequests.jsx
+++ react-axios/src/Components/HTTPRequests.jsx
@@ -9,10 +9,19 @@ export class HTTPRequests extends Component {
       posts: [],
     };
   }
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      console.log(response);
+      this.setState({
+        posts: response.data,
+      });
+    });
+  }
   render() {
     return (
       <div>
         <h2>Posts: </h2>
+        {JSON.stringify(this.state.posts)}
       </div>
     );
   }
