4aad230a7d31aa571a4ddb871b4c21057ad0bf83
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-14 16:54:53 +0300

    Add api response

--- react-axios/src/Components/HTTPPost.jsx
+++ react-axios/src/Components/HTTPPost.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import axios from "axios";
+
 export class HTTPPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: null,
+    };
+  }
+
   postToApi = () => {
     axios
       .post("https://jsonplaceholder.typicode.com/posts", {
@@ -9,13 +17,27 @@ export class HTTPPost extends Component {
         userId: 123,
       })
       .then((response) => {
-        console.log(response);
+        this.setState({
+          apiResponse: response.data,
+        });
       });
   };
+
   render() {
+    const { apiResponse } = this.state;
     return (
       <div>
         <button onClick={this.postToApi}>Create Post</button>
+        {apiResponse ? (
+          <div>
+            <h1>{apiResponse.title}</h1>
+            <p>Post ID: {apiResponse.id}</p>
+            <p>{apiResponse.body}</p>
+            <p>User ID: {apiResponse.userId}</p>
+          </div>
+        ) : (
+          <p>Please click the button above</p>
+        )}
       </div>
     );
   }
